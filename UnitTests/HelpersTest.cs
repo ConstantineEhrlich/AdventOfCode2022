e79de86474d202184ab942f1c96ca456c767830c
constantine <ferrumresistance@gmail.com>
2022-11-27 22:16:31 +0200

    tests

---
+++ UnitTests/HelpersTest.cs
@@ -0,0 +1,42 @@
+namespace UnitTests;
+using Helpers;
+
+[TestClass]
+public class HelpersTest
+{
+    [TestMethod]
+    public void GetDataForOneDay()
+    {
+        try
+        {
+            foreach (string s in new Data().ForYear(2021).ForDay(1))
+            {
+                Console.WriteLine(s);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            Assert.Fail();
+        }
+    }
+
+    [TestMethod]
+    public void GetDataWithoutDay()
+    {
+        try
+        {
+            foreach (var _ in new Data())
+            {
+                Assert.Fail();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+    
+    
+}
+
